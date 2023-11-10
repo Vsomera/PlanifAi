ba1b9aea2bdbd9c0a302fd787983d0caefaf5def
Vilmor Somera <vsomera77@outlook.com>
2023-11-09 17:39:31 -0800

    update database scripts

--- scripts/init-mongo.js
+++ scripts/init-mongo.js
@@ -1,14 +1,13 @@
 // Creates database and tables
-db = db.getSiblingDB('planifAi');
+db = db.getSiblingDB('project_db');
 db.createCollection('users');
 db.createCollection('plans');
 db.createCollection('itinerary');
 
-// Create users with roles if needed
-db.createUser({
-  user: 'root',
-  pwd: 'Password123!',
-  roles: [
-    { role: 'readWrite', db: 'planifAi' }
-  ]
-});
+// db.createUser({
+//   user: 'root',
+//   pwd: 'Password123!',
+//   roles: [
+//     { role: 'readWrite', db: 'project_db' }
+//   ]
+// });
