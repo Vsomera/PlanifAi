b5a9302e06293157e5dd4fb5126e81480e924bc3
Vsomera <vsomera77@outlook.com>
2023-11-11 00:26:06 -0800

    layout for planRoutes

--- server/routes/planRoutes.ts
+++ server/routes/planRoutes.ts
@@ -1,6 +1,14 @@
 import express from "express"
 const router = express.Router()
 
-router.post("/")
+// Create a plan
+// Change the name of the plan
+// Delete a plan
+
+// Add a place to the itinerary
+// Edit items the Itinerary
+// Delete items in the Itinerary
+
+// Set the date of a place in the itinerary
 
 module.exports = router
