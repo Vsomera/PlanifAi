571b4896fc5d30be0868c2febb57b30f3d04c785
Vsomera <vsomera77@outlook.com>
2023-11-21 13:54:46 -0800

    turfjs library + initial layout for fetching places nearby user location

--- client/src/api/travelAdv.ts
+++ client/src/api/travelAdv.ts
@@ -1,1 +1,6 @@
-// api stuff goes into here
+// import axios from "axios"
+
+export const fetchNearbyRestaurants = ( longitude : number, latitude : number ) => {
+    // returns a list of restaurants near a given coordinate
+    return { longitude, latitude}
+}
